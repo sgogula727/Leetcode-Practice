a26869a5ca48b44f7d2ef83ec31f8f09f4483ae9
Maanasa Gogula <gsmaanasa@gmail.com>
2026-05-21 21:30:19 -0400

    Time: 3 ms (94.16%) | Memory: 12.4 MB (18.23%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        if x <0:
+            return False
+
+        original = x
+        reversed = 0
+
+        while x > 0:
+            digit = x%10
+            reversed = reversed*10 +digit
+            x = x//10
+
+
+        return original == reversed
